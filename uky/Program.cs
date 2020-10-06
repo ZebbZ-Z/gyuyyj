83cd6fb02697df319b325bd98b6c52f61fa7abb1
Zebastian Zehlén <zebastian.zehlen@learnet.se>
2020-10-06 12:49:22 +0200

    j,gu

---
+++ uky/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
